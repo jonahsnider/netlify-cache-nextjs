cf033b1ba57c05111875da3b5303b136cb081dfa
Jonah Snider <jonah@jonah.pw>
2020-03-23 01:49:46 -0700

    fix: use Netlify build dir constant
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,24 @@ interface NetlifyUtils {
 	};
 }
 
+interface NetlifyConstants {
+  BUILD_DIR: string
+}
+
+interface NetlifyOpts {
+  utils: NetlifyUtils,
+  constants: NetlifyConstants
+}
+
 module.exports = {
 	name: 'cache-nextjs',
 	// Restore file/directory cached in previous builds.
 	// Does not do anything if:
 	//  - the file/directory already exists locally
 	//  - the file/directory has not been cached yet
-	async onPreBuild({utils}: {utils: NetlifyUtils}) {
-		await utils.cache.restore(joinPaths(__dirname, '.next'), {
-			digest: [joinPaths(__dirname, '.next', 'build-manifest.json')]
+	async onPreBuild({utils, constants}: NetlifyOpts) {
+		await utils.cache.restore(joinPaths(constants.BUILD_DIR, '.next'), {
+			digest: [joinPaths(constants.BUILD_DIR, '.next', 'build-manifest.json')]
 		});
 	},
 	// Cache file/directory for future builds.
@@ -24,9 +33,9 @@ module.exports = {
 	//  - the file/directory is already cached and its contents has not changed
 	//    If this is a directory, this includes children's contents
 	// Note that this will cache after the build, even if it fails, which fcould be unwanted behavior
-	async onPostBuild({utils}: {utils: NetlifyUtils}) {
-		await utils.cache.save(joinPaths(__dirname, '.next'), {
-			digest: [joinPaths(__dirname, '.next', 'build-manifest.json')]
+	async onPostBuild({utils, constants}: NetlifyOpts) {
+		await utils.cache.save(joinPaths(constants.BUILD_DIR, '.next'), {
+			digest: [joinPaths(constants.BUILD_DIR, '.next', 'build-manifest.json')]
 		});
 	}
 };
