10dcb9db1ef8b4674999c94bf2f2c7a797b1db61
Jonah Snider <jonah@jonah.pw>
2020-04-01 20:31:25 -0700

    fix: use relative path
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ interface NetlifyOpts {
 }
 
 function generateAbsolutePaths(
-	options: Pick<NetlifyOpts, 'inputs' | 'netlifyConfig'>
+	options: Pick<NetlifyOpts, 'inputs'>
 ): {
 	absolute: {
 		/** The absolute path to the build folder for Next.js. */
@@ -39,7 +39,7 @@ function generateAbsolutePaths(
 	const buildDirPathFromProject = options.inputs.build_dir_path ?? '.';
 
 	/** The absolute path to the build folder for Next.js. */
-	const absoluteBuildDirPath = joinPaths(options.netlifyConfig.build.base, buildDirPathFromProject, buildDirName, 'cache');
+	const absoluteBuildDirPath = joinPaths(buildDirPathFromProject, buildDirName, 'cache');
 	/** The absolute path to the build manifest Next.js uses. */
 	// I don't actually know if this build manifest has any relation to the cache folder
 	const manifestPath = joinPaths(absoluteBuildDirPath, '..', 'build-manifest.json');
@@ -59,8 +59,8 @@ module.exports = {
 	// Does not do anything if:
 	//  - the file/directory already exists locally
 	//  - the file/directory has not been cached yet
-	async onPreBuild({utils, netlifyConfig, inputs}: NetlifyOpts) {
-		const paths = generateAbsolutePaths({netlifyConfig, inputs});
+	async onPreBuild({utils, inputs}: NetlifyOpts) {
+		const paths = generateAbsolutePaths({inputs});
 		const success = await utils.cache.restore(paths.absolute.buildDir);
 
 		if (success) {
@@ -76,10 +76,10 @@ module.exports = {
 	//    If this is a directory, this includes children's contents
 	// Note that this will cache after the build, even if it fails, which fcould be unwanted behavior
 	async onPostBuild({utils, netlifyConfig, inputs}: NetlifyOpts) {
-		const paths = generateAbsolutePaths({netlifyConfig, inputs});
+		const paths = generateAbsolutePaths({inputs});
 
 		const success = await utils.cache.save(paths.absolute.buildDir, {
-			digests: [paths.absolute.manifest]
+      digests: [paths.absolute.manifest]
 		});
 
 		if (success) {
