5b4a163987cda1104fab9ba457188208fc2ef148
Jonah Snider <jonah@jonah.pw>
2020-09-08 13:15:32 -0700

    build(git): remvoe dist folder from git
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./netlify-plugin-cache-nextjs.cjs.production.min.js')
-} else {
-  module.exports = require('./netlify-plugin-cache-nextjs.cjs.development.js')
-}
